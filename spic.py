ddeabbced16ef305935119dbab6ef3db3da20cfa
deb847 <deb847@yandex.ru>
2012-07-08 14:01:19 +0700

    Type imagae

--- spic.py
+++ spic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from PIL import Image
+import imghdr
 
 path_folder = '/home/buhal/public_html/wordpress/wp-content/pic/'
 path_small_folder =  '/home/buhal/public_html/wordpress/wp-content/small_pic/'
@@ -13,6 +14,9 @@ def get_small_pic(name):
     size = im.size
 
     max_size =  sorted(size)[1]
+    type_im = imghdr.what(path_folder+name)
+
+    print type_im
 
     if max_size > 500:
 
@@ -26,14 +30,14 @@ def get_small_pic(name):
         new_im = im.resize(new_size)
 
 #        new_im.show()
-        new_im.save(path_small_folder+prefix+name,'JPEG')
+        new_im.save(path_small_folder+prefix+name,type_im)
 
     else:
         print "Clone pic"
-        im.save(path_small_folder+prefix+name,'JPEG')
+        im.save(path_small_folder+prefix+name,type_im)
 
     return prefix+name
 
 if __name__ == '__main__':
 
-    get_small_pic('pic3.jpg')
+    get_small_pic('a956599d-ec82-4b78-b61f-fab4fcbc14f2')
